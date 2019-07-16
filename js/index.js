f29427d1e5c18e7528fa4e0ca80d7e2629c9b698
m-mittal <hellomittal.mohit@gmail.com>
2019-07-16 09:46:55 +0530

    animation onLoad

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
-$('.button').click(function(){
+/* $('.button').click(function(){
   var buttonId = $(this).attr('id');
   $('#modal-container').removeAttr('class').addClass(buttonId);
   $('body').addClass('modal-active');
+}) */
+
+function onClickBtn(){
+  var buttonId = $('.button').attr('id');
+  $('#modal-container').removeAttr('class').addClass(buttonId);
+  $('body').addClass('modal-active');
+}
+
+
+$('document').ready(function(){
+  setTimeout(function(){
+    var elem = document.getElementsByClassName('button');
+    onClickBtn();
+  }, 300);
+
 })
 
 /*$('#modal-container').click(function(){
